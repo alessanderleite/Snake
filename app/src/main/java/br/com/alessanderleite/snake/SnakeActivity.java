41196526de6dd7d422ebb66e2eda01451eb566d8
alessanderleite <alessander.logistica@gmail.com>
2019-01-02 06:38:13 -0200

    declare an instance of SnakeEngine and code the onCreate methodin the SnakeActivity

--- app/src/main/java/br/com/alessanderleite/snake/SnakeActivity.java
+++ app/src/main/java/br/com/alessanderleite/snake/SnakeActivity.java
@@ -1,12 +1,28 @@
 package br.com.alessanderleite.snake;
 
 import android.app.Activity;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.view.Display;
 
 public class SnakeActivity extends Activity {
+
+    // Declare an instance of SnakeEngine
+    // We will code this soon
+    SnakeEngine snakeEngine;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Get the pixel dimensions of the screen
+        Display display = getWindowManager().getDefaultDisplay();
+
+        // Initialize the result into a Point object
+        Point size = new Point();
+        display.getSize(size);
+
+        // Create a new instance of SnakeEngine class
+        snakeEngine = new SnakeEngine(this, size);
     }
 }
